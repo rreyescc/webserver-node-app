0892db841f667e400aa4687fe4843add5ed840b1
Rafael Reyes <rreyescc@everis.com>
2024-04-17 23:55:41 -0500

    fix: modify type server port

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,6 +2,6 @@ import 'dotenv/config'
 import * as env from 'env-var'
 
 export const envs = {
-  SERVER_PORT: env.get('SERVER_PORT').required().asString(),
+  SERVER_PORT: env.get('SERVER_PORT').asString(),
   PUBLIC_PATH: env.get('PUBLIC_PATH').default('public').asString()
 }
